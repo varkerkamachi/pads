6691008f559f7d5dbbb2059a3a0dcab1b6b4e9c8
Andrew Cranston <andrew.cranston@gree.co.jp>
2013-07-09 10:31:47 -0700

    initial commit for pads tool

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Padmap::Application.config.secret_key_base = 'bce132cf8a76d1a8489dce6014cfb8e400bc922406f5390d33808e2f7a2e32d40256ee0f7128966df82622e96be6f2a2435140c3a5454b9ccb86b13d50c2f11f'
